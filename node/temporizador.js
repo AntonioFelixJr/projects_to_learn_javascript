585f444418b066d4471b06775dff8edd4a225439
Antonio Felix <antonio.felixjr@hotmail.com>
2019-09-06 01:24:06 -0300

    Node end

---
+++ node/temporizador.js
@@ -0,0 +1,24 @@
+const schedule = require('node-schedule')
+
+console.log('START!', new Date())
+
+const tarefa1 = schedule.scheduleJob('*/5 * 0 * * 5', function() {
+    console.log('Executando Tarefa 1!', new Date().getSeconds())
+})
+
+setTimeout(function() {
+    tarefa1.cancel()
+    console.log('Cancelamento Tarefa 1', new Date().getSeconds())
+}, 20000)
+
+// setImmediate
+// setInterval
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 0
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function() {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
